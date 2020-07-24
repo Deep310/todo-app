06f58054a192e372c6edb6c644f403684087e8f2
Deep310 <dparekh2019@gmail.com>
2020-07-24 13:56:35 -0400

    added config file

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,4 @@
-import firebase from 'firebase';
-
-const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyAIeH-XVN6k25eocZElu2msVCo58LQoZWI",
-    authDomain: "todo-app-ad013.firebaseapp.com",
-    databaseURL: "https://todo-app-ad013.firebaseio.com",
-    projectId: "todo-app-ad013",
-    storageBucket: "todo-app-ad013.appspot.com",
-    messagingSenderId: "945937620220",
-    appId: "1:945937620220:web:b67a332bc876a9cb0e4b79",
-    measurementId: "G-FM5P5GKX45"
-})
+import firebaseApp from './config'
 
 const db = firebaseApp.firestore();
 
